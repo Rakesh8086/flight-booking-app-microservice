f10eaeb39e31aba70f203cb2081ab77d1f52eca3
Rakesh8086 <rakeshteja7007@gmail.com>
2025-11-29 23:46:35 +0530

    Added get flight by Id and update inventory methods and helper function to map DTO to entity

--- flight-service/src/main/java/com/example/flight/service/FlightService.java
+++ flight-service/src/main/java/com/example/flight/service/FlightService.java
@@ -2,6 +2,7 @@ package com.example.flight.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.flight.dto.FlightDTO;
 import com.example.flight.entity.Flight;
@@ -13,4 +14,10 @@ public interface FlightService {
     
     // for searching flights
     List<Flight> searchFlights(String fromPlace, String toPlace, LocalDate scheduleDate);
+
+    // get flight by its ID
+    Optional<Flight> getFlightById(Long flightId);
+    
+    // to update flight seats after booking tickets
+    String updateFlightInventory(FlightDTO flightDto);
 }
--- flight-service/src/main/java/com/example/flight/service/impl/FlightServiceImpl.java
+++ flight-service/src/main/java/com/example/flight/service/impl/FlightServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.flight.service.impl;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,17 +34,7 @@ public class FlightServiceImpl implements FlightService {
         	throw new IllegalArgumentException("Arrival time must be after the departure time.");
         }
         
-    	Flight flight = new Flight();
-    	
-    	flight.setAirlineName(flightDto.getAirlineName());
-    	flight.setArrivalTime(flightDto.getArrivalTime());
-    	flight.setAvailableSeats(flightDto.getTotalSeats());
-    	flight.setDepartureTime(flightDto.getDepartureTime());
-    	flight.setPrice(flightDto.getPrice());
-    	flight.setScheduleDate(flightDto.getScheduleDate());
-    	flight.setFromPlace(flightDto.getFromPlace());
-    	flight.setToPlace(flightDto.getToPlace());
-    	flight.setTotalSeats(flightDto.getTotalSeats());
+    	Flight flight = FlightDtoToEntity(flightDto);
     	
         flightRepository.save(flight);
         
@@ -58,5 +49,35 @@ public class FlightServiceImpl implements FlightService {
                 scheduleDate, 
                 0 // show flights with 1 or more available seats
         );
+    }
+    
+    @Override
+    public Optional<Flight> getFlightById(Long flightId) {
+        return flightRepository.findById(flightId);
+    }
+    
+    @Override
+    public String updateFlightInventory(FlightDTO flightDto) {
+    	Flight flight = FlightDtoToEntity(flightDto);
+    	
+        flightRepository.save(flight);
+        
+        return "Inventory of Flight with Id " + flight.getId() + "been updated.";
+    }
+    
+    private Flight FlightDtoToEntity(FlightDTO flightDto) {
+    	Flight flight = new Flight();
+    	
+    	flight.setAirlineName(flightDto.getAirlineName());
+    	flight.setArrivalTime(flightDto.getArrivalTime());
+    	flight.setAvailableSeats(flightDto.getTotalSeats());
+    	flight.setDepartureTime(flightDto.getDepartureTime());
+    	flight.setPrice(flightDto.getPrice());
+    	flight.setScheduleDate(flightDto.getScheduleDate());
+    	flight.setFromPlace(flightDto.getFromPlace());
+    	flight.setToPlace(flightDto.getToPlace());
+    	flight.setTotalSeats(flightDto.getTotalSeats());
+    	
+    	return flight;
     }
 }
